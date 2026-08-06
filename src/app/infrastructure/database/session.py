a48246da82e7b14df1567d7feb45eb776cd56be4
Kohár Zsombor <zsombor.kohar@gmail.com>
2026-08-06 22:37:48 +0200

    docker setup

--- src/app/infrastructure/database/session.py
+++ src/app/infrastructure/database/session.py
@@ -1,12 +1,10 @@
+import os
 from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import sessionmaker, Session
 
-DATA_BASE_URL = ""
-
-if DATA_BASE_URL == "":
-    raise NotImplementedError("Database URL is not set up")
+DATA_BASE_URL = os.getenv("DATABASE_URL")
 
 engine: Engine = create_engine(DATA_BASE_URL)
 
-SessionLocal: sessionmaker[Session] = sessionmaker(engine)
+SessionLocal: sessionmaker[Session] = sessionmaker(bind=engine, autoflush=False)
 
---
+++ src/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.infrastructure.database.base import Base
+from app.infrastructure.database.session import engine
+from app.api.routes.reservations import router as reservation_router
+from app.api.routes.parking_spot import router as spot_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Parking System")
+
+@app.get("/", tags=["Health"])
+def health_check():
+    return {"status": "ok"}
+
+app.include_router(reservation_router)
+app.include_router(spot_router)

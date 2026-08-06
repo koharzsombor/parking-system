e85694b7155cc59d5bf82c95bbb837b70e2b00fd
Kohár Zsombor <zsombor.kohar@gmail.com>
2026-08-06 12:27:24 +0200

    Added checking to create reservation use case

--- src/app/application/use_cases/create_reservation.py
+++ src/app/application/use_cases/create_reservation.py
@@ -1,23 +1,37 @@
+from sqlalchemy.sql.functions import user
+
 from app.application.interfaces.reservation_repository import ReservationRepository
+from app.domain.models.parking_spot import ParkingSpot
 from app.domain.models.reservation import Reservation
 from uuid import uuid4, UUID
 from datetime import datetime
 
+from app.domain.models.user import User
+from app.domain.rules.reservation_availability_rule import ReservationAvailabilityRule
+from app.domain.rules.reservation_eligibility_rule import ReservationEligibilityRule
+
 
 class CreateReservation:
     def __call__(self,
-                user_id: UUID,
-                spot_id: int,
+                reserver: User,
+                spot: ParkingSpot,
                 reservation_repository: ReservationRepository,
                 start: datetime,
-                end: datetime):
+                end: datetime,
+                availability_rules: list[ReservationAvailabilityRule],
+                eligibility_rules: list[ReservationEligibilityRule]):
 
         new_reservation : Reservation = Reservation(
             id=uuid4(),
-            spot_id=spot_id,
+            spot_id=spot.id,
             start_time=start,
             end_time=end,
-            user_id=user_id
+            user_id=reserver.id
         )
 
-        reservation_repository.save(new_reservation)
+        if all(
+            rule.check(reservation_repository.get_by_spot(spot.id), start, end) for rule in availability_rules
+        ) and all(
+            rule.check(reserver, spot) for rule in eligibility_rules
+        ):
+            reservation_repository.save(new_reservation)

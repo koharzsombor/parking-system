45e043a902f50751222a80c0de663791f2fb0bde
Kohár Zsombor <zsombor.kohar@gmail.com>
2026-08-07 02:59:55 +0200

    get use cases di

--- src/app/di/use_cases.py
+++ src/app/di/use_cases.py
@@ -4,8 +4,11 @@ from app.application.interfaces.spot_repository import SpotRepository
 from app.application.interfaces.user_repository import UserRepository
 from app.application.use_cases.cancel_reservation import CancelReservation
 from app.application.use_cases.create_reservation import CreateReservation
+from app.application.use_cases.query_reservation import QueryReservation
+from app.application.use_cases.query_spot import QuerySpot
 from app.application.use_cases.query_spot_reservations import QuerySpotReservations
 from app.application.use_cases.query_spots import QuerySpots
+from app.application.use_cases.query_user import QueryUser
 from app.di.repositories import get_reservation_repository, get_spot_repository, get_user_repository
 from app.domain.rules.handicap_rule import HandicapRule
 from app.domain.rules.overlap_rule import OverlapRule
@@ -41,4 +44,25 @@ def get_query_spots(
 ) -> QuerySpots:
     return QuerySpots(
         spot_repository
+    )
+
+def get_query_spot(
+    spot_repository: SpotRepository = Depends(get_spot_repository)
+) -> QuerySpot:
+    return QuerySpot(
+        spot_repository
+    )
+
+def get_query_reservation(
+    reservation_repository: ReservationRepository = Depends(get_reservation_repository)
+) -> QueryReservation:
+    return QueryReservation(
+        reservation_repository
+    )
+
+def get_query_user(
+        user_repository: UserRepository = Depends(get_user_repository)
+) -> QueryUser:
+    return QueryUser(
+        user_repository
     )
